2581c742faea7d535f1831ef9aa05e1c001ccf05
trandrew <19401994+trandrew96@users.noreply.github.com>
2023-04-13 17:16:29 -0700

    improve footer spacing

--- src/Footer.js
+++ src/Footer.js
@@ -7,61 +7,60 @@ import instagramLogo from "./img/icon-instagram.svg";
 export default function Footer() {
   return (
     <div className="bg-verydarkblue text-white py-10">
-      <div className="max-w-screen-lg flex mx-auto md:flex-row flex-col text-center md:text-left">
+      <div className="max-w-screen-lg flex md:flex-row flex-col text-center md:text-left mx-auto">
         <div>
-          <a href="#" className="mx-auto inline-block mb-10">
+          <a href="#" className="mx-auto inline-block mb-10 md:mx-4">
             <img src={logo} />
           </a>
         </div>
-        <div className="md:ml-auto md:mr-20 mb-10 md:mb-0">
+
+        {/* Features */}
+        <div className="md:ml-auto mb-14 flex flex-col md:mr-16 gap-4 w-fit mx-auto">
           <h4 className="mb-4">Features</h4>
           <a href="#" className="text-gray hover:text-cyan">
             Link Shortening
           </a>
-          <br />
           <a href="#" className="text-gray hover:text-cyan">
             Branded Links
           </a>
-          <br />
           <a href="#" className="text-gray hover:text-cyan">
             Analytics
           </a>
-          <br />
         </div>
-        <div className="md:mr-20 mb-10 md:mb-0">
-          <h4 className="mb-4">Resources</h4>
+
+        {/* Resources */}
+        <div className="mb-14 flex flex-col md:mr-16 gap-4 w-fit mx-auto md:ml-0">
+          <h4>Resources</h4>
           <a href="#" className="text-gray hover:text-cyan">
             Blog
           </a>
-          <br />
           <a href="#" className="text-gray hover:text-cyan">
             Developers
           </a>
-          <br />
           <a href="#" className="text-gray hover:text-cyan">
             Support
           </a>
-          <br />
         </div>
-        <div className="md:mr-20 mb-10 md:mb-0">
-          <h4 className="mb-4">Company</h4>
+
+        {/* Company */}
+        <div className="mb-14 flex flex-col md:mr-16 gap-4 w-fit mx-auto md:ml-0">
+          <h4>Company</h4>
           <a href="#" className="text-gray hover:text-cyan">
             About
           </a>
-          <br />
           <a href="#" className="text-gray hover:text-cyan">
             Our Team
           </a>
-          <br />
           <a href="#" className="text-gray hover:text-cyan">
             Careers
           </a>
-          <br />
           <a href="#" className="text-gray hover:text-cyan">
             Contact
           </a>
         </div>
-        <div className="flex mx-auto md:mx-0">
+
+        {/* Socials */}
+        <div className="flex md:mr-4 mx-auto w-fit">
           <a href="#" className="mr-3">
             <img src={facebookLogo} />
           </a>
